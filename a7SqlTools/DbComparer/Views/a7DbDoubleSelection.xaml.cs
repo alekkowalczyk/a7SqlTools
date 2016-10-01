35b7dba6291462046fc1b383c584086d4fc1fc46
Alek Kowalczyk <alek.kowalczyk.7@gmail.com>
2016-10-01 14:48:29 +0200

    Version 0.9.0-beta

--- a7SqlTools/DbComparer/Views/a7DbDoubleSelection.xaml.cs
+++ a7SqlTools/DbComparer/Views/a7DbDoubleSelection.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using a7SqlTools.Annotations;
 using a7SqlTools.Utils;
 using Microsoft.SqlServer.Management.Common;
 
@@ -19,14 +22,37 @@ namespace a7SqlTools.DbComparer.Views
     /// <summary>
     /// Interaction logic for a7DbDoubleSelection.xaml
     /// </summary>
-    public partial class a7DbDoubleSelection : Window
+    public partial class a7DbDoubleSelection : Window, INotifyPropertyChanged
     {
+        private string _db1;
+        private string _db2;
         public List<string> DbList { get; private set; }
-        public string Db1 { get; set; }
-        public string Db2 { get; set; }
+        public bool IsOkEnabled { get; private set; }
+        public string Db1
+        {
+            get { return _db1; }
+            set
+            {
+                _db1 = value;
+                IsOkEnabled = !string.IsNullOrWhiteSpace(_db1) && !string.IsNullOrWhiteSpace(_db2);
+                OnPropertyChanged(nameof(IsOkEnabled));
+            }
+        }
+
+        public string Db2
+        {
+            get { return _db2; }
+            set
+            {
+                _db2 = value;
+                IsOkEnabled = !string.IsNullOrWhiteSpace(_db1) && !string.IsNullOrWhiteSpace(_db2);
+                OnPropertyChanged(nameof(IsOkEnabled));
+            }
+        }
 
         public a7DbDoubleSelection(List<string> dbList)
         {
+            IsOkEnabled = false;
             DbList = dbList;
             InitializeComponent();
         }
@@ -35,6 +61,14 @@ namespace a7SqlTools.DbComparer.Views
         {
             DialogResult = true;
             this.Close();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
